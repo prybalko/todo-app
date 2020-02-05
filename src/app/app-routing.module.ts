7599870a7fe96858c88a96fc65251bce06c01098
Pavel Rybalko <paul.rybalko@gmail.com>
2020-02-05 23:03:57 +0700

    add filtering based on url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {TodosComponent} from './todos/todos.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: TodosComponent
-  },
-  {
-    path: 'active',
-    component: TodosComponent
-  },
-  {
-    path: 'completed',
-    component: TodosComponent
-  }
+  {path: '', component: TodosComponent},
+  {path: 'completed', component: TodosComponent},
+  {path: 'active', component: TodosComponent},
+  {path: 'all', redirectTo: '', pathMatch: 'full'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule],
   providers: []
 })
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,15 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {Todo} from '../todo';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnChanges{
+
+  filteredTodos: Todo[];
 
   @Input()
   todos: Todo[];
@@ -17,7 +20,19 @@ export class TodoListComponent {
   @Output()
   toggleComplete: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  ngOnChanges() {
+    const path = this.router.routerState.snapshot.url;
+    if (path === '/active') {
+      this.filteredTodos = this.todos.filter(todo => !todo.complete);
+    } else if (path === '/completed') {
+      this.filteredTodos = this.todos.filter(todo => todo.complete);
+    } else {
+      this.filteredTodos = this.todos;
+    }
+  }
+
+  constructor(private router: Router) {
+  }
 
   onToggleTodoComplete(todo: Todo) {
     this.toggleComplete.emit(todo);
