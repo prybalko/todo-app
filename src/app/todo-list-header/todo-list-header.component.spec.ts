2cc68fd141b7efa184302df3a1492f91130d05b7
Pavel Rybalko <pavel.rybalko@quantumsoft.ru>
2020-02-10 12:41:13 +0700

    add list item tests

--- src/app/todo-list-header/todo-list-header.component.spec.ts
+++ src/app/todo-list-header/todo-list-header.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListHeaderComponent } from './todo-list-header.component';
 import {FormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
 
 describe('TodoListHeaderComponent', () => {
   let component: TodoListHeaderComponent;
@@ -23,5 +24,27 @@ describe('TodoListHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit addTodo on Enter', () => {
+    spyOn(component.add, 'emit');
+
+    component.newTodo.title = 'some title';
+    const input = fixture.debugElement.query(By.css('.new-todo'));
+    input.triggerEventHandler('keyup.enter', null);
+    fixture.detectChanges();
+
+    expect(component.add.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit if input is empty', () => {
+    spyOn(component.add, 'emit');
+
+    component.newTodo.title = '  ';
+    const input = fixture.debugElement.query(By.css('.new-todo'));
+    input.triggerEventHandler('keyup.enter', null);
+    fixture.detectChanges();
+
+    expect(component.add.emit).not.toHaveBeenCalled();
   });
 });
--- src/app/todo-list-item/todo-list-item.component.spec.ts
+++ src/app/todo-list-item/todo-list-item.component.spec.ts
@@ -1,8 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TodoListItemComponent } from './todo-list-item.component';
+import {TodoListItemComponent} from './todo-list-item.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Todo} from '../todo';
+import {By} from '@angular/platform-browser';
 
 describe('TodoListItemComponent', () => {
   let component: TodoListItemComponent;
@@ -19,11 +20,67 @@ describe('TodoListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoListItemComponent);
     component = fixture.componentInstance;
-    component.todo = new Todo();
+    component.todo = new Todo({title: 'Hello 1', complete: false});
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit remove event on button click', () => {
+    spyOn(component.remove, 'emit');
+
+    const button = fixture.debugElement.query(By.css('.destroy'));
+    button.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    expect(component.remove.emit).toHaveBeenCalledWith(component.todo);
+  });
+
+  it('should emit toggleComplete event on checkbox click', () => {
+    spyOn(component.toggleComplete, 'emit');
+
+    const button = fixture.debugElement.query(By.css('.toggle'));
+    button.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    expect(component.toggleComplete.emit).toHaveBeenCalledWith(component.todo);
+  });
+
+  it('should enter editing mode on double click', () => {
+    const label = fixture.debugElement.query(By.css('.list-item label'));
+    label.triggerEventHandler('dblclick', {});
+    fixture.detectChanges();
+
+    expect(component.editing).toEqual(true);
+    const inputField = fixture.debugElement.query(By.css('input[type="text"]'));
+    expect(inputField.nativeElement.value).toEqual(component.todo.title);
+  });
+
+  it('should trigger remove event if after editing todo is empty', () => {
+    spyOn(component.remove, 'emit');
+    component.editing = true;
+    fixture.detectChanges();
+
+    const form = fixture.debugElement.query(By.css('form'));
+    component.todoForm.setValue('    ');
+    form.triggerEventHandler('ngSubmit', null);
+    fixture.detectChanges();
+
+    expect(component.remove.emit).toHaveBeenCalledWith(component.todo);
+  });
+
+  it('should trigger update event after editing', () => {
+    spyOn(component.update, 'emit');
+    component.editing = true;
+    fixture.detectChanges();
+
+    const form = fixture.debugElement.query(By.css('form'));
+    component.todoForm.setValue('any-value');
+    form.triggerEventHandler('ngSubmit', null);
+    fixture.detectChanges();
+
+    expect(component.update.emit).toHaveBeenCalledWith(component.todo);
   });
 });
